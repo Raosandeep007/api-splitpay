53c3b900dea6a417dc18a623aa77f3d6aad3bbfe
Raosandeep007 <raosandeep@Raos-MacBook-Pro.local>
2025-01-14 15:53:43 +0530

    Add predeploy script, refactor routes for cleaner structure, and update TypeScript configuration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,19 +7,16 @@ import { fileRoutes } from "./file";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.redirect("/splitpay");
-});
-
-router.get("/splitpay", (req, res) => {
+  // res.redirect("/splitpay");
   res.send("Welcome to Splitpay API");
 });
 
-router.use("/splitpay/auth", authRoutes);
-router.use("/splitpay/file", fileRoutes);
+router.use("/auth", authRoutes);
+router.use("/file", fileRoutes);
 
 // Under this all routes are protected
 // TODO: uncomment this line after implementing authMiddleware
 // router.use(authMiddleware);
-router.use("/splitpay/user", userRoutes);
+router.use("/user", userRoutes);
 
 export default router;
